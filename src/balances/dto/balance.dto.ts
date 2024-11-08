007f7f189c84bee30fd4edf6221643de858c46c4
dorhaimovich <dorhaim765@gmail.com>
2024-11-08 17:58:34 +0200

    feat: added input validators

--- src/balances/dto/balance.dto.ts
+++ src/balances/dto/balance.dto.ts
@@ -1,8 +1,17 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsNotEmpty, IsString, Min } from 'class-validator';
 
 export class CreateBalanceDto {
+  @IsString()
+  @IsNotEmpty()
   currency: string;
+
+  @IsString()
+  @IsNotEmpty()
   asset: string;
+
+  @Min(0)
+  @IsNotEmpty()
   amount: number;
 }
 

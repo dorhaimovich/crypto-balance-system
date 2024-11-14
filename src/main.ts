f511061ff97570b2d07d247c7ed8b48583335984
dorhaimovich <dorhaim765@gmail.com>
2024-11-14 12:50:45 +0200

    feat: added app setups

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { HttpExceptionFilter } from './shared/exceptions/http-exceptions.filter'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.enableCors(); // add configuration to my front site only
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
 

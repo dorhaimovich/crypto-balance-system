9fd93c9aefa37a7f4eaebd45e2a5a7001a762aab
dorhaimovich <dorhaim765@gmail.com>
2024-11-14 11:18:23 +0200

    feat: added type validation

--- src/balances/dto/update-balance.dto.ts
+++ src/balances/dto/update-balance.dto.ts
@@ -1,7 +1,8 @@
-import { IsNotEmpty, Min } from 'class-validator';
+import { IsNotEmpty, IsNumber, Min } from 'class-validator';
 
 export class UpdateBalanceDto {
   @Min(0)
+  @IsNumber()
   @IsNotEmpty()
   amount: number;
 }

ab23f290c16eb20ea28e2459431496d1e7a939ce
dorhaimovich <dorhaim765@gmail.com>
2024-11-14 16:46:54 +0200

    feat: added logger module and created the service with log and error methods

---
+++ src/logger/logger.service.ts
@@ -0,0 +1,43 @@
+import { ConsoleLogger, Injectable } from '@nestjs/common';
+import * as fs from 'fs';
+import * as path from 'path';
+
+@Injectable()
+export class LoggerService extends ConsoleLogger {
+  async logToFile(
+    filename: string,
+    logType: string,
+    message: any,
+    ctx: string,
+  ) {
+    const formattedTimeStamp = Intl.DateTimeFormat('he-IL', {
+      dateStyle: 'short',
+      timeStyle: 'medium',
+      timeZone: 'Asia/Jerusalem',
+    }).format(new Date());
+    const formattedEntry = `[${formattedTimeStamp}] [${logType.toUpperCase()}] [${ctx}] ${message}\n`;
+
+    try {
+      const logsDir = path.resolve(process.cwd(), 'logs');
+      if (!fs.existsSync(logsDir)) {
+        await fs.promises.mkdir(logsDir);
+      }
+      await fs.promises.appendFile(
+        path.join(logsDir, filename),
+        formattedEntry,
+      );
+    } catch (e) {
+      if (e instanceof Error) console.error(e.message);
+    }
+  }
+
+  log(message: any, context: string = this.context) {
+    this.logToFile('logs.log', 'log', message, context);
+    super.log(message, context);
+  }
+
+  error(message: string | object, stackOrContext: string = this.context) {
+    this.logToFile('errors.log', 'error', message, stackOrContext);
+    super.error(message, stackOrContext);
+  }
+}

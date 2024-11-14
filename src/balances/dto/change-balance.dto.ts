c31a3a2ffb6dd7f520f8c3da8d179c024b9728bb
dorhaimovich <dorhaim765@gmail.com>
2024-11-14 11:15:18 +0200

    refactor: removed change balance dto

--- src/balances/dto/change-balance.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateBalanceDto } from './create-balance.dto';
-
-export class ChangeBalanceDto extends PartialType(CreateBalanceDto) {}

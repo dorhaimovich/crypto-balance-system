7df7c020c72eb7cffbf15473004506b9e505a2be
dorhaimovich <dorhaim765@gmail.com>
2024-11-14 12:50:11 +0200

    feat: added rate-limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,24 @@ import { BalancesModule } from './balances/balances.module';
 import { DatabaseModule } from './database/database.module';
 import { RatesModule } from './rates/rates.module';
 import { ConfigModule } from '@nestjs/config';
-
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { Throttlers } from './shared/constants/throttlers.options';
 @Module({
   imports: [
     BalancesModule,
     DatabaseModule,
     RatesModule,
     ConfigModule.forRoot(),
+    ThrottlerModule.forRoot(Throttlers.getAllThrottlers()),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}

323587b7710fe4eef8359ba334c21965c202bd20
dorhaimovich <dorhaim765@gmail.com>
2024-11-15 15:25:02 +0200

    feat: add rates controller

--- src/rates/rates.module.ts
+++ src/rates/rates.module.ts
@@ -3,9 +3,11 @@ import { RatesService } from './rates.service';
 import { DatabaseModule } from 'src/database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { HttpModule } from '@nestjs/axios';
+import { RatesController } from './rates.controller';
 @Module({
   imports: [DatabaseModule, ConfigModule, HttpModule],
   providers: [RatesService],
   exports: [RatesService],
+  controllers: [RatesController],
 })
 export class RatesModule {}

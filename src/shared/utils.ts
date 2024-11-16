2e151c29fc17c2858f637d5a88ecd34b2011f48e
dorhaimovich <dorhaim765@gmail.com>
2024-11-16 13:39:14 +0200

    refactor: added utillity for formattetimestamp

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,6 +1,8 @@
 import * as path from 'path';
 import * as fs from 'fs';
 
+export { getDirPath, getFormattedTimeStamp };
+
 const getDirPath = async (dirPath: string): Promise<string> => {
   const dataDir = path.resolve(process.cwd(), dirPath);
   if (!fs.existsSync(dataDir)) {
@@ -9,4 +11,10 @@ const getDirPath = async (dirPath: string): Promise<string> => {
   return dataDir;
 };
 
-export { getDirPath };
+const getFormattedTimeStamp = () => {
+  return Intl.DateTimeFormat('he-IL', {
+    dateStyle: 'short',
+    timeStyle: 'medium',
+    timeZone: 'Asia/Jerusalem',
+  }).format(new Date());
+};

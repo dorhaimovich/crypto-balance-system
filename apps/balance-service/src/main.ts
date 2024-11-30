c37530f6475f17e9d703a9c742687bd9c9caf445
dorhaimovich <dorhaim765@gmail.com>
2024-11-30 14:23:09 +0200

    feat: restore balance-service main functionality after monorepo changes

--- apps/balance-service/src/main.ts
+++ apps/balance-service/src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { BalanceServiceModule } from './balance-service.module';
+import { AllExceptionsFilter } from '@app/shared';
 
 async function bootstrap() {
   const app = await NestFactory.create(BalanceServiceModule);
-  await app.listen(process.env.port ?? 3000);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
+  app.enableCors();
+  app.setGlobalPrefix('api');
+  await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();

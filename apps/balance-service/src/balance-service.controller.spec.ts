c2c06310fa89d9c736a2f6ab693d81553144494b
dorhaimovich <dorhaim765@gmail.com>
2024-12-01 23:26:11 +0200

    refactor: class naming

--- apps/balance-service/src/balance-service.controller.spec.ts
+++ apps/balance-service/src/balance-service.controller.spec.ts
@@ -1,19 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { BalanceServiceController } from './balance-service.controller';
-import { BalanceServiceService } from './balance-service.service';
+import { BalanceController } from './balance-service.controller';
+import { BalanceService } from './balance-service.service';
 
 describe('BalanceServiceController', () => {
-  let balanceServiceController: BalanceServiceController;
+  let balanceServiceController: BalanceController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
-      controllers: [BalanceServiceController],
-      providers: [BalanceServiceService],
+      controllers: [BalanceController],
+      providers: [BalanceService],
     }).compile();
 
-    balanceServiceController = app.get<BalanceServiceController>(
-      BalanceServiceController,
-    );
+    balanceServiceController = app.get<BalanceController>(BalanceController);
   });
 
   describe('root', () => {
